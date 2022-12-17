0b7da0113712ba7a3be694f477f96de8f8e763b3
Brandon Martino <brandonmmartino1@gmail.com>
2022-12-17 15:22:46 -0600

    solved repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) return "ERROR"
+    let result = '';
+    const number = Math.floor(Math.random() * 1000);
+    
+    for (let i=0; i < num; i++) {
+        result += str;
+    }
+    return result;
 };
 
 // Do not edit below this line
