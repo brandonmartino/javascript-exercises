925dff7364ced74a70c265cf703335768bba8ef5
Brandon Martino <brandonmmartino1@gmail.com>
2022-12-17 15:50:54 -0600

    solved reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let charArray = str.split('');
+    charArray.reverse();
+    let reversedString = charArray.join('');
+    return reversedString;
+    
 };
 
 // Do not edit below this line
